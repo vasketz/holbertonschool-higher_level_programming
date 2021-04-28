5bdac7b7d755e2b80bad91f678ff74ecff7ea607
vasketz <anfevach@gmail.com>
2021-04-28 02:17:53 +0000

    Update task 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,4 @@ def islower(c):
     for char in c:
         if (ord(char) >= 97 and ord(char) <= 122):
             return True
-        elif (ord(char) >= 65 and ord(char) <= 90):
-            return False
-        else:
-            return False
+    return False
