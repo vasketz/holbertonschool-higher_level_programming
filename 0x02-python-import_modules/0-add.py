3ad969933b891f32f2f678ce237d44c27d507824
vasketz <anfevach@gmail.com>
2021-04-28 17:25:30 +0000

    Update task 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ add = __import__('add_0').add
 if __name__ == '__main__':
     a = 1
     b = 2
-    print(a, '+', b, '=', "{:d}".format(add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
