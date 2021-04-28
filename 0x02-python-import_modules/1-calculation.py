3d79dbfc2576683c7fbd2fd14b0f2095304e437f
vasketz <anfevach@gmail.com>
2021-04-28 19:36:37 +0000

    Update task 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,8 @@
 a = 10
 b = 5
 if __name__ == "__main__":
-    from calculator_1 import add
+    from calculator_1 import add, sub, mul, div
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    from calculator_1 import sub
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    from calculator_1 import mul
     print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-    from calculator_1 import div
     print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
