c7bfa33df0409be11874fd0d4bca1f189760bee9
vasketz <anfevach@gmail.com>
2021-05-06 01:45:29 +0000

    Update task 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = ""
+    for i in range(len(my_string)):
+        s = my_string[i]
+        if s == "c" or s == "C":
+            s = ""
+        new = new + s    
+    return new
