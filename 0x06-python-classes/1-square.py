0148e4cf8e255e4e446fd968a2f566dd6743d9c8
vasketz <anfevach@gmail.com>
2021-05-18 22:08:46 +0000

    Update task 2 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square():
+    """define size on intance of class square"""
+    def __init__(self, size):
+        """ inicialize the instance"""
+        self.__size = size
