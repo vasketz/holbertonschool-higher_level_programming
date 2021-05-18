4f1782792ef00de4b95d00fd085be0c0b4cb9e54
vasketz <anfevach@gmail.com>
2021-05-18 23:01:20 +0000

    Update task 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square():
+    """define size on intance of class square"""
+    def __init__(self, size=0):
+        """ inicialize the instance"""
+        if isinstance(size, int):
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Define an area of square"""
+        return self.__size ** 2
