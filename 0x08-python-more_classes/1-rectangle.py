00285e6eb2c7db8d7cb9defde21e387797ed1c72
vasketz <anfevach@gmail.com>
2021-05-20 22:40:29 +0000

    Update task 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,30 +6,15 @@ class Rectangle:
     """Creat a class rectangle"""
     def __init__(self, width=0, height=0):
         """initialize values"""
-        if isinstance(width, int):
-            self.__width = width
-            if width < 0:
-                raise ValueError("width must be >= 0")
-        else:
-            raise TypeError("width must be an integer")
         if isinstance(height, int):
             self.__height = height
             if height < 0:
                 raise ValueError("height must be >= 0")
         else:
             raise TypeError("height must be an integer")
-
-    @property
-    def width(self):
-        """Return getter of width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """setter of width"""
-        if isinstance(value, int):
-            self.__width = value
-            if value < 0:
+        if isinstance(width, int):
+            self.__width = width
+            if width < 0:
                 raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
@@ -49,3 +34,17 @@ class Rectangle:
         else:
             raise TypeError("height must be an integer")
 
+    @property
+    def width(self):
+        """Return getter of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        if isinstance(value, int):
+            self.__width = value
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
