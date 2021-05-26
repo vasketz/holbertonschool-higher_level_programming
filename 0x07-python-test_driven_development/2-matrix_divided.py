2895360bb2a676789a7de754c89c7bca6a73f68f
vasketz <anfevach@gmail.com>
2021-05-26 13:04:16 +0000

    Update task 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module that divide a matrix
+checks for integers and floats
+te list must have the same size
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    return a new matrix with integers or floats
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    size = len(matrix[0])
+    for lt in matrix:
+        aux = []
+        if size == len(lt):
+            for num in lt:
+                if type(num) is int or type(num) is float:
+                    aux.append(round(num / div, 2))
+                else:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_matrix.append(aux)
+        else:
+            raise TypeError("Each row of the matrix must have the same size")
+    return new_matrix
