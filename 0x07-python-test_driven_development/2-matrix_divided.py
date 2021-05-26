bd09e442bf1dd1238c6524053a659d10cb8595a1
vasketz <anfevach@gmail.com>
2021-05-26 13:08:18 +0000

    Fix pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,11 +11,13 @@ def matrix_divided(matrix, div):
     return a new matrix with integers or floats
     """
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     new_matrix = []
     size = len(matrix[0])
+    te = "matrix must be a matrix (list of lists) of integers/floats"
     for lt in matrix:
         aux = []
         if size == len(lt):
@@ -23,7 +25,7 @@ def matrix_divided(matrix, div):
                 if type(num) is int or type(num) is float:
                     aux.append(round(num / div, 2))
                 else:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError(te)
             new_matrix.append(aux)
         else:
             raise TypeError("Each row of the matrix must have the same size")
