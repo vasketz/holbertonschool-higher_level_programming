9431c59988942f3a64b17454966d4080dfe8240c
vasketz <anfevach@gmail.com>
2021-06-02 22:42:50 +0000

    Update task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+This is the modle documentation
+"""
+
+
+def to_json_string(my_obj):
+    """ return a json representation of an object """
+    return json.dumps(my_obj)
