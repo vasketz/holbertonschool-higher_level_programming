09c098ebd2e03047157693a89760113055c098b9
vasketz <anfevach@gmail.com>
2021-06-02 00:24:25 +0000

    Update task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module documentation is kind of class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if the object is an instance"""
+    return isinstance(obj, a_class)
