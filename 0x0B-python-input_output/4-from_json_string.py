113a97fbb64dd24e524b2ccd737988890609a15d
vasketz <anfevach@gmail.com>
2021-06-02 23:04:43 +0000

    Update task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the module documentation
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Return from object to json string"""
+    return json.loads(my_str)
