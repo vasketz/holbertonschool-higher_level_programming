be44bcf19d4cbcb5afe6b0544ed0814d663f2c1e
vasketz <anfevach@gmail.com>
2021-06-02 01:37:30 +0000

    Update task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module documentation of base geometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometric
+    """
+    def area(self):
+        """Method area not implemented"""
+        raise Exception("area() is not implemented")
