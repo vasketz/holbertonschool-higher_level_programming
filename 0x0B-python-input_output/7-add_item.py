901f31d6c59134afb4e78e16b150f9292c7f77d8
vasketz <anfevach@gmail.com>
2021-06-03 01:50:20 +0000

    Update task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-this is the module documentation of load, add, save
-"""
-import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
 
 
 arg = sys.argv[1:]
