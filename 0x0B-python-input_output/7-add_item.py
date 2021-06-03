0b77658fbfad85201278b692c5ac1a062e769015
vasketz <76267087+vasketz@users.noreply.github.com>
2021-06-03 11:45:01 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,12 +11,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 arg = sys.argv[1:]
 filename = "add_item.json"
 
-objec = load_from_json_file(filename)
+
+try:
+    objec = load_from_json_file(filename)
 for i in arg:
     objec.append(i)
 save_to_json_file(objec, filename)
-
-try:
-    load_from_json_dile(filename)
 except:
     save_to_jason_file(arg, filename)
