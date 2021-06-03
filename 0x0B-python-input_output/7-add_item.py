c506c318c06dce8443ce635927e40efcc7e42fb0
vasketz <anfevach@gmail.com>
2021-06-03 16:35:39 +0000

    Update task 7 with try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,19 @@
 This is the module documentation
 """
 import sys
-import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 arg = sys.argv[1:]
 filename = "add_item.json"
+
 objec = load_from_json_file(filename)
 for i in arg:
     objec.append(i)
-
 save_to_json_file(objec, filename)
+
+try:
+    load_from_json_dile(filename)
+except:
+    save_to_jason_file(arg, filename)
