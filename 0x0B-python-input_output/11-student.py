de6a5440c66c338febba50c417cb3363bbf8a654
vasketz <anfevach@gmail.com>
2021-06-04 02:36:42 +0000

    Update task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This is the module documentation
+"""
+
+
+class Student:
+    """define a class Student """
+    def __init__(self, first_name, last_name, age):
+        """instantiation of attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary"""
+        diccio = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        else:
+            for item in attrs:
+                if type(item) is not str:
+                    return self.__dict__
+                else:
+                    if item in self.__dict__:
+                        diccio[item] = self.__dict__[item]
+            return diccio
+
+    def reload_from_json(self, json):
+        """set attributes"""
+        for key in json:
+            setattr(self, key, json[key])
