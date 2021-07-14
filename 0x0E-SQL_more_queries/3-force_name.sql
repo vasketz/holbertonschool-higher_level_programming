1aee2c80a07d36434c8aeb8645536fb625e2f3d2
vasketz <anfevach@gmail.com>
2021-07-14 02:09:19 +0000

    Update task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates the table force_name on your MySQL server.
+-- this script creates a table with id and name
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
