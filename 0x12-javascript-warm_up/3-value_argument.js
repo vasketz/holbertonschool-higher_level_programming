e88de1bd8cb96b471ccb7c25d9efe5e1e0690605
vasketz <anfevach@gmail.com>
2021-08-02 23:22:33 +0000

    Update task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // this script prints the value pased throught argv
 const args = process.argv[2];
 if (!args) {
-  console.log('No arguments');
+  console.log('No argument');
 } else {
   console.log(args);
 }
