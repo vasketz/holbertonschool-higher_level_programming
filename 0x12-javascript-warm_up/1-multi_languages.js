ff7fcfa7ed44086cc77d2c07f83228332b0d6310
vasketz <anfevach@gmail.com>
2021-08-02 22:47:28 +0000

    Update semistandard task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // this script print tree lenguages
-let lang = "C is fun";
+let lang = 'C is fun';
 console.log(lang);
-lang = "Python is cool";
+lang = 'Python is cool';
 console.log(lang);
-lang = "JavaScript is amazing";
+lang = 'JavaScript is amazing';
 console.log(lang);
