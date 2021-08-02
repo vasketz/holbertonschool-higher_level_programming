8308c3907fc72d25940242b4ff3a11c34b734017
vasketz <anfevach@gmail.com>
2021-08-02 22:55:38 +0000

    Update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // this script prints if arguments are passed
-const argv_process = process.argv.slice(2).length;
-if (argv_process === 0) {
-    console.log("No argument");
-}
-else {
-    console.log("Argument found")
+const argvProcess = process.argv.slice(2).length;
+if (argvProcess === 0) {
+  console.log('No argument');
+} else if (argvProcess === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
