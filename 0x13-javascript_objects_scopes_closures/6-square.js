b10ac72259405570eea04b99aab1b9a6b05c4862
vasketz <anfevach@gmail.com>
2021-08-03 17:49:07 +0000

    Update task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// write a class Square that defines a square and inherits from square
+const Squar = require('./5-square');
+module.exports = class Square extends Squar {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let character = '';
+      let col, row;
+      for (col = 0; col < this.height; col++) {
+        for (row = 0; row < this.width; row++) {
+          character += 'C';
+        }
+        console.log(character);
+        character = '';
+      }
+    }
+  }
+};
