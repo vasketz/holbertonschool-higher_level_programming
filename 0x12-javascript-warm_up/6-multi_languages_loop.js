afea9c9c58fbe7f1feb4d1324fba62b0e74dce6b
vasketz <anfevach@gmail.com>
2021-08-03 00:23:51 +0000

    Update task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// this script prints all lenguages but using an array and a loop
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < langs.length) {
+  console.log(langs[i]);
+  i++;
+}
