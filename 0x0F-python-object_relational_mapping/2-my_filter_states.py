06d1a0c7e952f0c71b1bd02215fb18ad21cc19b2
root <root@7854324757b6>
2021-08-09 02:48:09 +0000

    Update tassk 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    crsor = db.cursor()
+    crsor.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY '{}'\
+    ORDER BY id".format(str(argv[4])))
+    rows = crsor.fetchall()
+    for row in rows:
+        print(row)
+    crsor.close()
+    db.close()
