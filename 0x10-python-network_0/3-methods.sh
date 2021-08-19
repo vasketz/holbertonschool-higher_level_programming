2f937b86f685f7c4b1fcdb47e1179df6947a7797
vasketz <anfevach@gmail.com>
2021-08-19 21:53:55 +0000

    Update task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # write a script that takes a url and display all http methods
-curl -s -I nocseteipsum.tech | grep Allow | cut -d" " -f2
+curl -s -I nocseteipsum.tech | grep Allow | cut -d" " -f2-
