4997b902104085745487b65261c8d64d84269e30
vasketz <anfevach@gmail.com>
2021-09-14 22:10:40 +0000

    Update task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,10 @@
 
 const https = require('https');
 
-https.get(process.argv[2], (res) => {
-  console.log('code: ', res.statusCode);
+https.get(process.argv[2], (data) => {
+  console.log('code:', data && data.statusCode);
+
+  data.on('error', (e) => {
+    console.error(e);
+  });
 });
