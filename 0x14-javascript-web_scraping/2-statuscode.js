fa10ebbd00e69bcbd94f9c58cb3af1373fbb3dc6
vasketz <anfevach@gmail.com>
2021-09-14 22:21:35 +0000

    Update 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,11 +2,12 @@
 // statusCode output from request
 
 const request = require('request');
+const url = process.argv[2]
 
-request(process.argv[2], (err, data) => {
+request(url, (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', data && data.statusCode);
+    console.log('code:', data.statusCode);
   }
 });
