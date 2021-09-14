99b8b174a1b0ec7485bb64d05611d2568cdf0ed4
vasketz <anfevach@gmail.com>
2021-09-14 21:42:28 +0000

    fix semistandard task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,10 @@
 
 const fs = require('fs');
 
-
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
-    }
+  }
 });
