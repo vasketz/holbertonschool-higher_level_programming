b6e71350b31e7fac0311453e9f63e0f1e592ac5c
vasketz <76267087+vasketz@users.noreply.github.com>
2021-09-14 20:24:59 -0500

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(url, id, (error, data, body) => {
+request(url, (error, data, body) => {
   if (error) {
     return console.error(error);
   } else {
-    console.log(JSON.parse(body).id);
+    console.log(JSON.parse(body).title);
   }
 });
